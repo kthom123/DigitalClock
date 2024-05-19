3134d823fc3f7e650f680015a7feaa1a9a1ddcec
Kyle Thomas <kylejwthomas@gmail.com>
2024-05-19 15:15:08 +0900

    Added code lines

--- Digital Clock/Form1.cs
+++ Digital Clock/Form1.cs
@@ -9,7 +9,10 @@ namespace Digital_Clock
 
         private void timer_Tick(object sender, EventArgs e)
         {
-
+            lblTime.Text = DateTime.Now.ToString("HH:mm");
+            lblSecond.Text = DateTime.Now.ToString("ss");
+            lblDate.Text = DateTime.Now.ToString("MM dd yyyy");
+            lblDay.Text = DateTime.Now.ToString("dddd");
         }
     }
 }
